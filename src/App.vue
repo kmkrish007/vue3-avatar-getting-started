070aa16fac07f58bac2f53110ce2cec6b464896d
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-08-17 17:33:38 +0530

    vue3-getting-started for Avatar

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id='element'>
+    <span class="e-avatar e-avatar-xlarge"></span>
+    <span class="e-avatar e-avatar-large"></span>
+    <span class="e-avatar"></span>
+    <span class="e-avatar e-avatar-small"></span>
+    <span class="e-avatar e-avatar-xsmall"></span>
+</div>
+</template>
+
+<script>
+import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+import "../node_modules/@syncfusion/ej2-layouts/styles/material.css";
+
+export default {
+    name: "App"
+}
+</script>
+<style>
+
+#element {
+    display: block;
+    width: 300px;
+    margin: 130px auto;
+    border-radius: 3px;
+    justify-content: center;
+}
+
+.e-avatar {
+    background-image: url(./images/pic01.png);
+    margin: 2px;
+}
+</style>
